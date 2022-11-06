473d153fcab0b69398ed029757942ff43b983ba5
김하영 <khy4018@gmail.com>
2022-11-06 16:45:11 +0900

    learn array.push in react way

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import {useState, useEffect} from "react"
 
-function Hello() {
-  useEffect(() => {
-    console.log('hi :)')
-    return () => console.log('bye :(')
-  }, [])
-
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing(prev => !prev)
+  const [toDo, setTodo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo === "") {
+      return
+    }
+    setTodo("")
+    setToDos((currentArray) => [toDo, ...currentArray])
+  }
+  console.log(toDos)
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'hide' : 'show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do ..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
