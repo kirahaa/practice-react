648664c1c00fea749b9cdf1beb6268c3a953dffa
김하영 <khy4018@gmail.com>
2022-11-06 16:27:49 +0900

    learn useEffect(when the component destroyed)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import {useState, useEffect} from "react"
 
-function App() {
-  const [counter, setValue] = useState(0)
-  const [keyword, setKeyword] = useState('')
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => {
-    setKeyword(event.target.value)
+function Hello() {
+  const byeFn = () => {
+    console.log('bye :<')
   }
+  const HiFn = () => {
+    console.log("created!")
+    return byeFn
+  }
+  useEffect(HiFn, [])
+  return <h1>Hello</h1>
+}
 
-  console.log('i run all the time')
-
-  useEffect(() => {
-    console.log('i run one time')
-  }, [])
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log('search for', keyword)
-    }
-  }, [keyword])
-
-  useEffect(() => {
-    console.log('i run when "counter" changes')
-  }, [counter])
-
-  useEffect(() => {
-    console.log('i run when keyword & counter changes')
-  }, [keyword, counter])
+function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing(prev => !prev)
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'hide' : 'show'}</button>
     </div>
   );
 }
