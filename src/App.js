32c20b56dc8c40609719caefa24626d4661dfdee
김하영 <khy4018@gmail.com>
2022-11-13 16:02:04 +0900

    Movie App part one

--- src/App.js
+++ src/App.js
@@ -9,26 +9,31 @@ function App() {
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
       )
     ).json();
-
     setMovies(json.data.movies);
     setLoading(false);
   };
 
-  console.log(movies)
-
   useEffect(() => {
     getMovies();
-    // fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   })
   }, [])
 
-
   return <div>
-    {loading ? <h1>Loading...</h1> : null}
+    {loading ? (<h1>Loading...</h1>) : (
+      <div>
+        {movies.map(movie => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} alt={movie.title}/>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map(g =>
+                <li key={g}>{g}</li>
+              )}
+            </ul>
+          </div>
+        ))}
+      </div>
+    )}
   </div>
 }
 
