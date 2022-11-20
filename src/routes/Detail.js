ef5c37fe9ea1533487cb88e689ff6ee263368f88
김하영 <khy4018@gmail.com>
2022-11-20 15:22:01 +0900

    Modify getMovie (useCallback)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,31 @@
 import {useParams} from "react-router-dom"
-import {useEffect} from "react";
+import {useEffect, useState, useCallback} from "react";
 
 function Detail() {
   const {id} = useParams()
+  const [movie, setMovie] = useState({})
 
-  const getMovie = async () => {
-    const json = await(
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(json)
-  }
+  const getMovie = useCallback(async () => {
+    const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    const json = await response.json()
+    setMovie(json.data.movie)
+  }, [id])
 
   useEffect(() => {
     getMovie()
-  }, [])
+  }, [getMovie])
 
-  return <h1>Detail</h1>
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <img src={movie.background_image} alt={movie.title}/>
+      <div>
+        <div>Intro : {movie.description_intro}</div>
+        <div>Runtime : {movie.runtime}</div>
+        <div>Rating : {movie.rating}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Detail
